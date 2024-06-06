f7243e022bfdfc3d7c263bcd9c508b7d86a76038
Mikhail Elanov <Elanov1991@Gmail.com>
2024-06-06 09:57:28 +0700

    Add unfollowing functional

--- controllers/follow-controller.js
+++ controllers/follow-controller.js
@@ -37,7 +37,32 @@ const FollowController = {
     }
   },
   unfollowUser: async (req, res) => {
-    res.send("unfollowUser");
+    const { followerId } = req.body;
+    const userId = req.user.userId;
+
+    try {
+      const follows = await prisma.follows.findFirst({
+        where: {
+          AND: [{ followerId: userId }, { followerId }],
+        },
+      });
+
+      if (!follows) {
+        return res
+          .status(404)
+          .json({ error: "Вы не подписаны на этого пользователя" });
+      }
+
+      await prisma.follows.delete({
+        where: { id: follows.id },
+      });
+
+      res.status(201).json({ message: "Вы отписались" });
+    } catch (error) {
+      console.error("Unfollow error", error);
+
+      return res.status(500).json({ error: "Internal server error" });
+    }
   },
 };
 
